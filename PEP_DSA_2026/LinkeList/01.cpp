f50d0a3b6ea2638931e8216202d163e50f327219
Ramesh <your-email@example.com>
2026-07-03 16:55:23 +0530

    initate commit

--- PEP_DSA_2026/LinkeList/01.cpp
+++ PEP_DSA_2026/LinkeList/01.cpp
@@ -28,7 +28,49 @@ void insertAtEnd(Node* &head, int val){
 
 }
 
+void insertAtBegning(Node* &head, int val){
+    Node* node= new Node(val);
+
+    node->next=head;
+    head=node;
+}
+
+void printEvenNodes(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        if(temp->data%2==0){
+            cout<<temp->data<<" ";
+        }
+        temp=temp->next;
+    }
+}
+
+void printNodes(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+void insertAtPosition(Node* &head, int pos, int val){
+    Node* node = new Node(val);
+    Node* temp= head;
+
+    if(pos==1){
+        node->next=head;
+        head=node;
+        return;
+    }
+    for(int i=1; i<pos-1 && pos!= NULL;i++){
+        temp=temp->next;
+    }
+    node->next=temp->next;
+    temp->next=node;
+}
 int main(){
     Node* head = NULL;
     insertAtEnd(head, 1);
+
+    printEvenNodes(head);
 }
